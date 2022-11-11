2e13a9aee3082cd3adb50ce7a3e7c46520ba2ce4
Davro2005 <116736664+Davro2005@users.noreply.github.com>
2022-11-11 17:03:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+//electrochasi1
+//let n=+prompt("int");
+//let h= Math.floor(n/60)%24;
+//let m=n%60;
+//console.log(+h);
+//console.log(+m);
+//electrochasi2
+//let n=+prompt("int");
+//let h= Math.floor(n/3600)%24;
+//let m= Math.floor(n/60)%60;
+//let s=n%60;
+//console.log(+h+":"+Math.floor(m/10)+m%10+":"+Math.floor(s/10)+s%10);
+//pirozhok
+//let a=+prompt("int");
+//let b=+prompt("int2");
+//let c=+prompt("int3");
+//let s=(a*100)+b;
+//let d=(b%100);
+//console.log(Math.floor(s/100)%60)
+//task1
+//let n=+prompt("int");
+//let a=Math.floor(n/1000);
+//let b=Math.floor(n/100)%10;
+//let c=Math.floor(n/10)%10;
+//let d=n%10;
+//a===d&&c===b?console.log("YES"):console.log("NO");
+//task2
+//let cnt=0;
+//for(let i=1;i<=3;i++)
+//{    
+   //let a=+prompt("inter")
+    //if(a>0)
+   //{
+        //cnt++;
+    //}
+   
+//}
+//console.log(+cnt);
+//task3
+//let n=+prompt("inter")
+//for(let i=1;i<=n;i++)
+//{
+//    console.log(i+" ");
+//}
+
+
+//for(let i=n;i>=1;i--)
+//{
+//    console.log(i+" ");
+//}
+//task4
+let a=+prompt("inter");
+let b=+prompt("inter");
+for(let i=a;i>=b;i++)
+{
+    let sum=sum+i*i;
+}
+console.log(+sum);
+
+
+
